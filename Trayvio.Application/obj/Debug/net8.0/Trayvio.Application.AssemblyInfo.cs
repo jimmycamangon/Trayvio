bc718c0168f411a27d73e730034e78a4838baad1
zhask09 <73562149+jimmycamangon@users.noreply.github.com>
2026-01-11 11:37:38 +0800

    Fix: align AutoMapper DI extensions to net8

--- Trayvio.Application/obj/Debug/net8.0/Trayvio.Application.AssemblyInfo.cs
+++ Trayvio.Application/obj/Debug/net8.0/Trayvio.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Trayvio.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32f06674965a0fca538aa1840e3aac9ad74c2de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed13685f8a6736533a9ee39d7b0c1d8f9f7ce816")]
 [assembly: System.Reflection.AssemblyProductAttribute("Trayvio.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Trayvio.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
